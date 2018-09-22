170ac2aaffa1bf5be31f2946c24a8f10d8a41987
nmatsuo63 <nmatsuo.63@gmail.com>
2018-09-22 15:33:22 +0900

    First commit.

---
+++ FormApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace FormApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("ios=bc21da20-9a5a-4382-8dac-c6c532939d35;",
+                            typeof(Analytics), typeof(Crashes), typeof(Distribute));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
